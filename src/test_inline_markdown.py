be0a9f03a9054e166aec45a15924c1621250799d
calcielisa-creator <calcielisa@gmail.com>
2026-05-19 15:47:06 +0200

    test_text_to_textnodes moved into test_inline_markdown

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,7 +1,15 @@
 import unittest
 
 from textnode import TextType, TextNode
-from inline_markdown import split_nodes_delimiter, split_nodes_image, split_nodes_link, extract_markdown_images, extract_markdown_links
+from inline_markdown import (
+    split_nodes_delimiter,
+    split_nodes_image,
+    split_nodes_link,
+    extract_markdown_images,
+    extract_markdown_links,
+    text_to_textnodes
+)
+
 
 class TestSplitNodesDelimiter(unittest.TestCase):
     def test_one_delimiter_pair_code(self):
@@ -215,3 +223,67 @@ class TestExtractMarkdown(unittest.TestCase):
         self.assertListEqual([("to youtube", "https://www.youtube.com/@bootdotdev")], matches_links)
 
 
+class TestTextToTextNodes(unittest.TestCase):
+    def test_text_to_tn(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        result_nodes = text_to_textnodes(text)
+        self.assertEqual(result_nodes, [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+            ]
+        )
+    
+    def test_text_to_tn_another_order(self):
+        text = "This is text with an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and an _italic_ word and a `code block` and a [link](https://boot.dev)"
+
+        result_nodes = text_to_textnodes(text)
+        self.assertEqual(result_nodes, [
+            TextNode("This is text with an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+            ]
+        )
+
+    def test_text_to_tn_no_texttypes(self):
+
+        text = "This is text with no italic, no code block and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        result_nodes = text_to_textnodes(text)
+        self.assertEqual(result_nodes, [
+            TextNode("This is text with no italic, no code block and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+            ]
+        )
+    
+    def test_text_to_tn_no_image_no_link(self):
+        text = "This is **text** with an _italic_ word and a `code block` and no images or links"
+        result_nodes = text_to_textnodes(text)
+        self.assertEqual(result_nodes, [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and no images or links", TextType.TEXT),
+            ]
+        )
+
+    def test_text_to_tn_no_text(self):
+        text = ""
+        result_nodes = text_to_textnodes(text)
+        self.assertEqual(result_nodes, [])
